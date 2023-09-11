aba2613d4531862826d40280f3bcc945bae890d8
worrapimpeem <128181081+worrapimpeem@users.noreply.github.com>
2023-09-11 20:49:16 +0700

    Update import.py

--- import.py
+++ import.py
@@ -1,11 +1,11 @@
 import json
 
-# Specify the JSON file name
-json_file = "data.json"
+# Specify the input JSON file name
+input_json_file = "input_data.json"
 
 try:
-    # Open and read the JSON file
-    with open(json_file, 'r') as file:
+    # Open and read the input JSON file
+    with open(input_json_file, 'r') as file:
         json_data = file.read()
 
     # Replace single quotes with double quotes
@@ -17,10 +17,16 @@ try:
     # Serialize the dictionary back to valid JSON format with proper indentation
     formatted_json = json.dumps(data_dict, indent=2)
 
-    # Print the cleaned and formatted JSON
-    print(formatted_json)
+    # Specify the output JSON file name
+    output_json_file = "formatted_data.json"
+
+    # Save the formatted JSON data to a new JSON file
+    with open(output_json_file, 'w') as outfile:
+        outfile.write(formatted_json)
+
+    print(f"Formatted JSON data saved to {output_json_file}")
 
 except FileNotFoundError:
-    print(f"The file '{json_file}' was not found.")
+    print(f"The file '{input_json_file}' was not found.")
 except json.JSONDecodeError as e:
     print(f"Error decoding JSON: {e}")
