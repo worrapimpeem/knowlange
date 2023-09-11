cd245840fa350e77bc3f3cbc53db5669fa8e07fc
worrapimpeem <128181081+worrapimpeem@users.noreply.github.com>
2023-09-12 01:48:08 +0700

    Update import.py

--- import.py
+++ import.py
@@ -1,24 +1,25 @@
 import json
 
 # Define the input and output file names
-input_file_name = 'k.json'
+input_file_name = 'input.json'
 output_file_name = 'output.json'
 
 # Read data from the input JSON file
 with open(input_file_name, 'r') as input_file:
     input_data = json.load(input_file)
 
-# Flatten the JSON structure
-output_data = {
-    "name": input_data["name"],
-    "age": input_data["age"],
-    "street": input_data["address"]["street"],
-    "city": input_data["address"]["city"],
-    "state": input_data["address"]["state"]
-}
+# Process the data and create the desired output structure
+output_data = []
+for item in input_data:
+    flat_item = {
+        "name": item["name"],
+        "substreet": item["person"]["street"]["substreet"],
+        "city": item["person"]["street"]["city"]
+    }
+    output_data.append(flat_item)
 
-# Write the flattened data to the output JSON file
+# Write the processed data to the output JSON file
 with open(output_file_name, 'w') as output_file:
     json.dump(output_data, output_file, indent=4)
 
-print(f'Flattened data saved to "{output_file_name}"')
+print(f'Processed data saved to "{output_file_name}"')
