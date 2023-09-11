70edbcb156e1145b5ea761caebc5ca00939e0263
worrapimpeem <128181081+worrapimpeem@users.noreply.github.com>
2023-09-11 23:26:52 +0700

    Update import.py

--- import.py
+++ import.py
@@ -1,1 +1,16 @@
-=TEXTJOIN("",TRUE,IFERROR(MID(B5,ROW(INDIRECT("1:"&LEN(B5)),1)*1," ")
+import json
+
+# Load the JSON data from a file
+with open("data.json", "r") as json_file:
+    data = json.load(json_file)
+
+# Define the criteria for deletion (e.g., "Age" equals 30)
+criteria = "Age"
+value_to_delete = 30
+
+# Iterate through the JSON array and remove matching objects
+data = [item for item in data if item.get(criteria) != value_to_delete]
+
+# Save the modified data back to the JSON file
+with open("data.json", "w") as json_file:
+    json.dump(data, json_file, indent=4)
