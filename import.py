da3790259dd27f75544895acd188bdbc9461f43a
worrapimpeem <128181081+worrapimpeem@users.noreply.github.com>
2023-09-17 16:02:48 +0700

    Update import.py

--- import.py
+++ import.py
@@ -1,25 +1,17 @@
 import json
 
-# Define the input and output file names
-input_file_name = 'input.json'
-output_file_name = 'output.json'
-
-# Read data from the input JSON file
-with open(input_file_name, 'r') as input_file:
-    input_data = json.load(input_file)
-
-# Process the data and create the desired output structure
-output_data = []
-for item in input_data:
-    flat_item = {
-        "name": item["name"],
-        "substreet": item["person"]["street"]["substreet"],
-        "city": item["person"]["street"]["city"]
+# Original JSON data
+data = {
+    'name': "'dick'John'put'",
+    'age': 30,
+    'address': {
+        'street': '123 Main St',
+        'city': 'New York'
     }
-    output_data.append(flat_item)
+}
 
-# Write the processed data to the output JSON file
-with open(output_file_name, 'w') as output_file:
-    json.dump(output_data, output_file, indent=4)
+# Serialize the data to a properly formatted JSON string
+formatted_json = json.dumps(data, indent=4)
 
-print(f'Processed data saved to "{output_file_name}"')
+# Print the formatted JSON
+print(formatted_json)
